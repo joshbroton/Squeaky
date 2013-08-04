5969bf3cc921394a3a9aa3135609145529dc714b
Josh Broton <joshbroton@gmail.com>
2013-08-03 21:59:04 -0500

    Code archive page for social posts

---
+++ archive-sq_socialpost.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+    <section class="main clearfix">
+        <section class="content" role="main">
+            <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class('article clearfix'); ?> role="article">
+                    <header class="article-header">
+                        <h1>
+                            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                        </h1>
+                        <aside class="byline vcard">
+                            <time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(get_option('date_format')); ?></time> by <span class="author"><?php the_author_posts_link(); ?></span>
+                        </aside>
+                    </header>
+                    <section class="article-content clearfix">
+                        <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail();
+                        }
+                        the_content();
+                        ?>
+                    </section>
+                    <footer class="article-footer">
+                        <section class="categories">
+                            Categories: <?php the_terms( $post->ID, 'sq_socialpost_categories', '', ', ', ' ' ); ?>
+                        </section>
+                    </footer>
+                    <hr />
+                </article>
+            <?php endwhile; ?>
+                <nav class="wp-prev-next">
+                    <div class="prev-link">
+                        <?php echo get_next_posts_link('&laquo; Older Entries') ?>
+                    </div>
+                    <div class="next-link">
+                        <?php echo previous_posts_link('Newer Entries &raquo;') ?>
+                    </div>
+                </nav>
+            <?php else : ?>
+                <article class="post-404">
+                    <header class="article-header">
+                        <h2>
+                            Oops! Post not found.
+                        </h2>
+                    </header>
+                    <section class="article-content clearfix">
+                        Something has gone very, very, VERY wrong. Can you make sure you clicked on the right thing?
+                    </section>
+                    <footer class="article-footer">
+                        <p>
+                            You've gotten this error message from the index.php file in the template.
+                        </p>
+                    </footer>
+                </article>
+            <?php endif; ?>
+        </section>
+        <?php get_sidebar(); ?>
+    </section>
+
+<?php get_footer(); ?>
